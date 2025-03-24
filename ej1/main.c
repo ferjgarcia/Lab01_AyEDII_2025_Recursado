79c80787ba870da18d1d4874b8beb00f89fb02e6
FernandoJGarcia <fernandojaviergarcia04@gmail.com>
2025-03-23 23:04:52 -0300

    Termine ejercicio 1

--- ej1/main.c
+++ ej1/main.c
@@ -58,6 +58,9 @@ int main(int argc, char *argv[]) {
   int copy[MAX_SIZE];
   array_copy(copy, array, length);
 
+  /* show the initial array in the screen */
+  array_dump(array, length);
+
   /* do the actual sorting */
   insertion_sort(array, length);
 
--- ej1/sort.c
+++ ej1/sort.c
@@ -8,11 +8,22 @@
 
 
 static void insert(int a[], unsigned int i, unsigned int length) {
-    /* needs implementation */
+    unsigned int j = i;
+
+    while (length > j && j > 0 && goes_before(a[j], a[j-1]))
+    {
+        swap(a,j-1,j);
+        array_dump(a,length);
+        j--;
+    }
+    
 }
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1; i < length; ++i) {
         insert(a, i, length);
+        assert(array_is_sorted(a, i));
     }
 }
+
+
