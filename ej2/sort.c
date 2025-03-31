81d7232a970b879178d0013da4171ef68150b677
FernandoJGarcia <fernandojaviergarcia04@gmail.com>
2025-03-30 21:24:51 -0300

    ejercico 2 terminado

--- ej2/sort.c
+++ ej2/sort.c
@@ -8,12 +8,15 @@
 
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
- /* needs implementation */
+    unsigned int ppiv;
 
-    /* no implementes partition, ya está implementado en sort_helpers.o
-       (no se puede leer, pero en sort_helpers.h vas a encontrar información
-        para saber cómo usarlo)
-    */
+    if (der > izq)
+    {
+        ppiv = partition(a,izq,der);
+        quick_sort_rec(a,izq,ppiv);
+        quick_sort_rec(a,ppiv+1,der);
+    }
+    
 }
 
 void quick_sort(int a[], unsigned int length) {
