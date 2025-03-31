32770f88646ac3b6b69fd2bfa566e4d4cc970c4e
FernandoJGarcia <fernandojaviergarcia04@gmail.com>
2025-03-30 23:09:28 -0300

    lab1 terminado

--- ej5/fixstring.c
+++ ej5/fixstring.c
@@ -3,17 +3,65 @@
 
 #include "fixstring.h"
 
+unsigned int fstring_length(fixstring s)
+{
+    unsigned int i = 0;
 
-unsigned int fstring_length(fixstring s) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    return i;
 }
 
-bool fstring_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+bool fstring_eq(fixstring s1, fixstring s2)
+{
+    bool res = true;
+    unsigned int i = 0;
+
+    if (fstring_length(s1) != fstring_length(s2))
+    {
+        return res = false;
+    }
+
+    while (s1[i] != '\0' && s2[i] != '\0' && res)
+    {
+        res = s1[i] == s2[i];
+        i++;
+    }
+
+    return res;
 }
 
-bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+bool fstring_less_eq(fixstring s1, fixstring s2)
+{
+    bool res = false;
+    unsigned int i = 0;
+
+    if (fstring_length(s1) > 0u && fstring_length(s2) > 0u)
+    {
+        if (fstring_length(s1) < fstring_length(s2) && s1[0] <= s2[0])
+        {
+            return res = true;
+        }
+
+        while (s1[i] != '\0' && s2[i] != '\0')
+        {
+            if (s1[i] < s2[i] && s1[i] != s2[i])
+            {
+                return res = true;
+            }
+            else if (s1[i] > s2[i])
+            {
+                return res = false;
+            }
+
+            i++;
+        }
+    }
+
+    return res;
 }
 
 void fstring_set(fixstring s1, const fixstring s2) {
@@ -27,10 +75,9 @@ void fstring_set(fixstring s1, const fixstring s2) {
 
 void fstring_swap(fixstring s1,  fixstring s2) {
     fixstring aux;
-    /*
-     * COMPLETAR
-     *
-     */
+    fstring_set(aux,s1);
+    fstring_set(s1,s2);
+    fstring_set(s2,aux);
 }
 
 
--- ej5/sort_helpers.c
+++ ej5/sort_helpers.c
@@ -8,18 +8,14 @@
 #include "fixstring.h"
 
 void swap(fixstring a[], unsigned int i, unsigned int j) {
-    /*
-     * COMPLETAR
-     *
-     */
+    fixstring aux;
+    fstring_set(aux,a[i]);
+    fstring_set(a[i],a[j]);
+    fstring_set(a[j],aux);
 }
 
 bool goes_before(fixstring x, fixstring y) {
-    /*
-     * COMPLETAR
-     *
-     */
-    return false;
+    return fstring_less_eq(x,y);
 }
 
 bool array_is_sorted(fixstring array[], unsigned int length) {
